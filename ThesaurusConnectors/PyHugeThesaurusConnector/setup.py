aaa0c01d6a4f39efb70371ecffb058f87a25fc75
tttthomasssss <th0mas.ko6er@gmail.com>
2014-08-31 16:01:10 +0100

    First Release or so

--- ThesaurusConnectors/PyHugeThesaurusConnector/setup.py
+++ ThesaurusConnectors/PyHugeThesaurusConnector/setup.py
@@ -1,3 +1,5 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -7,7 +9,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-	name='sample',
+	name='pyhugeconnector',
 
 	version='0.1.1',
 
@@ -15,11 +17,11 @@ setup(
 	long_description=long_description,
 
 	# The project's main homepage.
-	url='https://github.com/pypa/sampleproject',
+	url='https://github.com/tttthomasssss/nlp-utils/tree/master/ThesaurusConnectors/PyHugeThesaurusConnector',
 
 	# Author details
-	author='The Python Packaging Authority',
-	author_email='pypa-dev@googlegroups.com',
+	author='tttthomasssss',
+	author_email='tttthomasssss@gmail.com',
 
 	# Choose your license
 	license='MIT',
@@ -30,19 +32,19 @@ setup(
 		#   3 - Alpha
 		#   4 - Beta
 		#   5 - Production/Stable
-		'Development Status :: 3 - Alpha',
+		'Development Status :: 4 - Beta',
 
 		# Indicate who your project is intended for
 		'Intended Audience :: Developers',
-		'Topic :: Software Development :: Build Tools',
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering :: Artificial Intelligence',
+		'Topic :: Software Development :: Libraries :: Python Modules',
 
 		# Pick your license as you wish (should match "license" above)
 		'License :: OSI Approved :: MIT License',
 
 		# Specify the Python versions you support here. In particular, ensure
 		# that you indicate whether you support Python 2, Python 3 or both.
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.6',
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.2',
@@ -51,7 +53,7 @@ setup(
 	],
 
 	# What does your project relate to?
-	keywords='sample setuptools development',
+	keywords='nlp bighugethesaurus thesaurus',
 
 	# You can just specify the packages manually here if your project is
 	# simple. Or you can use find_packages().
@@ -61,27 +63,5 @@ setup(
 	# project is installed. For an analysis of "install_requires" vs pip's
 	# requirements files see:
 	# https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-	install_requires=['peppercorn'],
-
-	# If there are data files included in your packages that need to be
-	# installed, specify them here.  If using Python 2.6 or less, then these
-	# have to be included in MANIFEST.in as well.
-	package_data={
-		'sample': ['package_data.dat'],
-	},
-
-	# Although 'package_data' is the preferred approach, in some case you may
-	# need to place data files outside of your packages.
-	# see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-	# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-	data_files=[('my_data', ['data/data_file'])],
-
-	# To provide executable scripts, use entry points in preference to the
-	# "scripts" keyword. Entry points provide cross-platform support and allow
-	# pip to create the appropriate form of executable for the target platform.
-	entry_points={
-		'console_scripts': [
-			'sample=sample:main',
-		],
-	},
+	install_requires=['requests>=2.4.0'],
 )
